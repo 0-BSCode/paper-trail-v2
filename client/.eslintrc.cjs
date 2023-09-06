c41f84f73cba8201e80e4fc279e52dacb7c890c8
0-BSCode <bryanipsanchez@gmail.com>
2023-09-06 21:34:16 +0800

    Initialize prettier and eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -2,9 +2,13 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
