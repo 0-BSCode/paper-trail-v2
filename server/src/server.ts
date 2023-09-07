2d872f59b9e35ba9173ff257b53b140ffea61855
0-BSCode <bryanipsanchez@gmail.com>
2023-09-07 21:05:52 +0800

    Revert "feat: containerized environment with HMR"
    This reverts commit 5f6ff0fda3ba68d41080b9a61c51c8431380e798.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,12 @@
-import express, { Express, Request, Response } from 'express';
-require('dotenv').config();
+import express, {Express, Request, Response} from 'express';
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, this is Express asd TypsScript');
+app.get('/', (req: Request, res: Response)=>{
+    res.send('Hello, this is Express asd TypeScript');
 });
 
-app.listen(port, () => {
-  console.log(`[Server]: I am running at http://localhost:${port}`);
+app.listen(port, ()=> {
+console.log(`[Server]: I am running at http://localhost:${port}`);
 });
