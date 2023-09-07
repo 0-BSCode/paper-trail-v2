2f6a69e008511d32068c2787efe107d925c7af2e
0-BSCode <bryanipsanchez@gmail.com>
2023-09-07 20:27:02 +0800

    Test prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express, Request, Response } from "express"
 
 const app: Express = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, this is Express + TypeScript')
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello, this is Express + TypeScript")
 })
 
 app.listen(port, () => {
