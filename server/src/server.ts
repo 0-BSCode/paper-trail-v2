9a26b6a9d95af0c1a5ad49d2511221bf10e7d36c
0-BSCode <bryanipsanchez@gmail.com>
2023-09-07 20:20:01 +0800

    Commit with prettier and eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express'
 
-const app: Express = express();
-const port = 3000;
+const app: Express = express()
+const port = 3000
 
-app.get('/', (req: Request, res: Response)=>{
-    res.send('Hello, this is Express asd TypeScript');
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, this is Express + TypeScript')
+})
 
-app.listen(port, ()=> {
-console.log(`[Server]: I am running at http://localhost:${port}`);
-});
+app.listen(port, () => {
+  console.log(`[Server]: I am running at http://localhost:${port}`)
+})
