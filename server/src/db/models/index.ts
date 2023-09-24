724215e45b02594a36298a638f7165b04aad1df9
0-BSCode <bryanipsanchez@gmail.com>
2023-09-24 19:14:26 +0800

    Refactor db setup

--- server/src/db/models/index.ts
+++ server/src/db/models/index.ts
@@ -1,5 +1,4 @@
 import sequelize from "../../config/db.config";
-import Sequelize from "sequelize";
 import { User } from "./user.model";
 import { RefreshToken } from "./refresh-token.model";
 import { Role } from "./role.model";
@@ -7,17 +6,6 @@ import { UserRole } from "./user-role.model";
 import { Document } from "./document.model";
 import { DocumentUser } from "./document-user.model";
 
-sequelize.addModels([User, RefreshToken, Role, UserRole, Document, DocumentUser]);
-
-const db = {
-  Sequelize,
-  sequelize,
-  User,
-  RefreshToken,
-  Role,
-  UserRole,
-  Document,
-  DocumentUser
+export const setupDb = (): void => {
+  sequelize.addModels([User, RefreshToken, Role, UserRole, Document, DocumentUser]);
 };
-
-export default db;
