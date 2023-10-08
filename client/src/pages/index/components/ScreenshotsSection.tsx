fbc5b753877f50a3c7f0a2913db0d03c2456e9fc
Luzefiru <ian.deejay.iv@gmail.com>
2023-10-08 22:52:36 +0800

    Refactor styles and typography

--- client/src/pages/index/components/ScreenshotsSection.tsx
+++ client/src/pages/index/components/ScreenshotsSection.tsx
@@ -1,28 +1,36 @@
-import Assistance from '@src/assets/assistance-picture.png';
-import Notifications from '@src/assets/notifications-picture.png';
+import Assistance from '@src/assets/assistance-screenshot.png';
+import Notifications from '@src/assets/notifications-screenshot.png';
 
 function ScreenshotsSection(): JSX.Element {
   return (
-    <section className="flex flex-col justify-center px-24 my-8 mt-5 h-[calc(100lvh-64px)]">
-      <div className="flex justify-center gap-4 items-center">
-        <img src={Assistance} alt="Girl Riding a Rocket Ship with a Thumbs-up" />
+    <section className="flex flex-col justify-center gap-24 px-24 my-32">
+      <div className="flex items-center justify-center gap-16">
+        <img
+          className="rounded-lg shadow-[0_8px_64px_16px_rgba(0,0,0,0.1)]"
+          src={Assistance}
+          alt="A Chatroom User Interface For Sending Messages"
+        />
         <div className="flex flex-col">
-          <h2 className="text-5xl font-bold leading-[4rem]">One-on-one assistance</h2>
-          <p className="max-w-prose">
+          <h2 className="mb-4 text-5xl font-bold">One-on-one assistance</h2>
+          <p className="leading-8">
             You can conveniently communicate with CISCO officers via our chat system to address and resolve your issues.
             Any messages sent will be forwarded to the parties involved in the ticket.
           </p>
         </div>
       </div>
-      <div className="flex justify-center gap-4 items-center">
+      <div className="flex items-center justify-between gap-16">
         <div className="flex flex-col">
-          <h2 className="text-5xl font-bold leading-[4rem] ">Email notifications</h2>
-          <p className="max-w-prose justify-start">
-            All app notifications are automatically sent to your registered email addresses, ensuring easy access to
-            information even when you're not in the app.
+          <h2 className="mb-4 text-5xl font-bold">Email notifications</h2>
+          <p className="justify-start leading-8">
+            All app notifications are automatically sent to your registered email addresses, <br /> ensuring easy access
+            to information even when you&lsquo;re not in the app.
           </p>
         </div>
-        <img src={Notifications} alt="Girl Riding a Rocket Ship with a Thumbs-up" />
+        <img
+          className="rounded-lg shadow-[0_8px_64px_16px_rgba(0,0,0,0.1)]"
+          src={Notifications}
+          alt="A Notification Drawer with Multiple Messages"
+        />
       </div>
     </section>
   );
