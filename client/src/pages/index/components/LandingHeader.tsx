3f5a5bb7dd7d5b9683fa3558f3b165926d913e0f
Luzefiru <ian.deejay.iv@gmail.com>
2023-10-08 20:30:42 +0800

    Refactor logo to be H1 for semantic HTML

--- client/src/pages/index/components/LandingHeader.tsx
+++ client/src/pages/index/components/LandingHeader.tsx
@@ -8,7 +8,10 @@ function LandingHeader(): JSX.Element {
 
   return (
     <header className="h-[64px] bg-black px-5 text-white flex justify-between shadow-md sticky top-0 w-full">
-      <WordmarkLogo />
+      <h1 className="flex">
+        <WordmarkLogo />
+      </h1>
+
       <div className="flex items-center">
         <ContainedButton onClick={handleRegisterButtonClick}>Register Now</ContainedButton>
       </div>
