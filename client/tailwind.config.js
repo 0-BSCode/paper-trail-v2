11fcbeb2eaef9d77bbfacdcc2da7280cebd79a55
Luzefiru <ian.deejay.iv@gmail.com>
2023-10-08 18:52:06 +0800

    Add font stack & apply globally

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Helvetica', 'Arial', 'Tahoma', 'sans-serif'],
+      },
       maxWidth: {
         container: '85rem',
         '1/2': '50%',
