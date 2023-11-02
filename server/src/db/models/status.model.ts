4e41426570d01307c2f15ce45c679c102a97a4e8
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-02 13:32:22 +0800

    added document id foreign key to status model

--- server/src/db/models/status.model.ts
+++ server/src/db/models/status.model.ts
@@ -1,11 +1,20 @@
-import { Table, Column, Model, DataType, AllowNull } from "sequelize-typescript";
+import { Table, Column, Model, DataType, AllowNull, BelongsTo, ForeignKey } from "sequelize-typescript";
 import StatusEnum from "../../types/enums/status-enum";
+import { Document } from "./document.model";
 
 @Table({ tableName: "comment", underscored: true })
 class Status extends Model {
   @AllowNull(false)
   @Column(DataType.ENUM("DRAFT", "REVIEW_REQUESTED", "REVIEW", "CHANGES_REQUESTED", "RAISED", "RESOLVED"))
   name!: StatusEnum;
+
+  @ForeignKey(() => Document)
+  @AllowNull(false)
+  @Column(DataType.NUMBER)
+  documentId!: number;
+
+  @BelongsTo(() => Document)
+  document!: Document;
 
   @AllowNull(false)
   @Column(DataType.STRING)
