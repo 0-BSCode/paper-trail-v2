7a4b06937afd7faf200cb9ca18652d099a39fe64
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-02 21:35:59 +0800

    Fixed api endpoints

--- server/src/routes/document.route.ts
+++ server/src/routes/document.route.ts
@@ -17,7 +17,7 @@ router.post("/:id/share", authenticate, shareValidator.create, shareController.c
 router.delete("/:documentId/share/:userId", authenticate, shareController.delete);
 router.get("/:documentId/comment", authenticate, commentController.getDocumentComments);
 router.post("/:documentId/comment", authenticate, commentController.createComment);
-router.get("/document/status", authenticate, statusController.getAll);
-router.get("/document/:id/status", authenticate, statusController.getStatus);
-router.put("/document/:id/status", authenticate, statusController.updateStatus);
+router.get("/status", authenticate, statusController.getAll);
+router.get("/:id/status", authenticate, statusController.getStatus);
+router.put("/:id/status", authenticate, statusController.updateStatus);
 export default router;
