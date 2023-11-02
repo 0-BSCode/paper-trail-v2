ccbb2a164e0df7266ef35c815e08bf2e43d90ffb
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-02 12:51:09 +0800

    Create StatusEnum for Status model

---
+++ server/src/types/enums/status-enum.ts
@@ -0,0 +1,10 @@
+enum StatusEnum {
+  DRAFT = "draft",
+  REVIEW_REQUESTED = "review-requested",
+  REVIEW = "review",
+  CHANGES_REQUESTED = "changes-requested",
+  RAISED = "raised",
+  RESOLVED = "resolved"
+}
+
+export default StatusEnum;
