77a723c45969e98f3afb314531463b140d53bc25
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-15 20:42:41 +0800

    Create Table for Tickets page

--- client/src/pages/all-tickets/components/AssignedTickets.tsx
+++ client/src/pages/all-tickets/components/AssignedTickets.tsx
@@ -1,6 +1,9 @@
+import TicketsTable from './TicketsTable';
+
 const AssignedTickets = (): JSX.Element => (
   <div className="w-[90%] h-[45%] bg-white-100 flex flex-col gap-2 px-[2rem] py-[1.3rem]">
     <h1 className="m-0 text-xl font-bold">Your Tickets</h1>
+    <TicketsTable />
   </div>
 );
 
