f2e2cd44801b8d4fd2cd0d9c1b70a0722bd104c9
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-15 16:08:40 +0800

    Create Assigned Tickets and Unassigned tickets components

---
+++ client/src/pages/all-tickets/components/UnassignedTickets.tsx
@@ -0,0 +1,7 @@
+const UnassignedTickets = (): JSX.Element => (
+  <div className="w-[90%] h-[40%] bg-white-100 flex flex-col gap-2 px-[2rem] py-[1.3rem]">
+    <h1 className="m-0 text-3xl font-bold">Ticket Tracker</h1>
+  </div>
+);
+
+export default UnassignedTickets;
--- client/src/pages/all-tickets/index.tsx
+++ client/src/pages/all-tickets/index.tsx
@@ -1,10 +1,16 @@
+import AssignedTickets from './components/AssignedTickets';
 import Header from './components/Header';
+import UnassignedTickets from './components/UnassignedTickets';
 
 // TODO (Sam): Fill in
 const AllTicketsPage = (): JSX.Element => {
   return (
     <div className="h-full">
       <Header />
+      <div className="flex flex-col items-center justify-center h-full gap-5">
+        <AssignedTickets />
+        <UnassignedTickets />
+      </div>
     </div>
   );
 };
