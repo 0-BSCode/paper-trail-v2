884605299354b2871202a0c3d77d26db4dc375ae
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-22 21:32:43 +0800

    Add utils and types

---
+++ client/src/utils/convertToTitleCase.ts
@@ -0,0 +1,7 @@
+const convertToTitleCase = (str: string): string => {
+  return str.replace(/_/g, ' ').replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+
+export default convertToTitleCase;
