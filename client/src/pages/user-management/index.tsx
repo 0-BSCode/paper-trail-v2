01267da0ee7ebff572c7f144a3ba25d674972fbc
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-24 22:57:14 +0800

    Create Header

--- client/src/pages/user-management/index.tsx
+++ client/src/pages/user-management/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import Header from './components/Header';
 
-// TODO (Fitz): Fill in
 const UserManagementPage = (): JSX.Element => {
-  return <div>UserManagementPage</div>;
+  return (
+    <div className="flex flex-col h-fit bg-[#f5f5f5]">
+      <Header />
+      <div className="flex flex-col items-center justify-center h-full gap-5 py-8"></div>
+    </div>
+  );
 };
 
 export default UserManagementPage;
