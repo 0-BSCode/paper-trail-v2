cad40f45859cc8e913cc6c092435b9a3277b6681
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-24 23:07:10 +0800

    Create TableContainer

---
+++ client/src/pages/user-management/components/TableContainer.tsx
@@ -0,0 +1,37 @@
+import { Input } from 'antd';
+
+import { useState } from 'react';
+import DropDown from './DropDown';
+import StatusEnum from '@src/types/enums/status-enum';
+
+const TableContainer = (): JSX.Element => {
+  const [filtered, setFiltered] = useState<boolean>(false);
+  const [titleFilter, setTitleFilter] = useState<string>('');
+  const [dropDownFilter, setDropDownFilter] = useState<StatusEnum>(StatusEnum.ALL);
+
+  return (
+    <div className="w-[90%] h-[45%] bg-white-100 flex flex-col gap-2 px-[2rem] py-[1.3rem]">
+      <h1 className="m-0 mb-5 text-xl font-bold border-2 border-black">Users</h1>
+      <div className="flex flex-col ">
+        <div className="flex gap-5">
+          <div className="flex  flex-col justify-start w-[40%]">
+            <p className="my-2 font-semibold ">Search by Email</p>
+            <Input
+              onChange={(e) => {
+                setTitleFilter(e.target.value);
+              }}
+              placeholder="Title"
+              style={{ width: '100%' }}
+            />
+          </div>
+          <div className="flex flex-col mb-3">
+            <p className="my-2 font-semibold ">Status</p>
+            <DropDown dropDownFilter={dropDownFilter} setDropDownFilter={setDropDownFilter} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TableContainer;
--- client/src/pages/user-management/index.tsx
+++ client/src/pages/user-management/index.tsx
@@ -1,10 +1,13 @@
 import Header from './components/Header';
+import TableContainer from './components/TableContainer';
 
 const UserManagementPage = (): JSX.Element => {
   return (
     <div className="flex flex-col h-fit bg-[#f5f5f5]">
       <Header />
-      <div className="flex flex-col items-center justify-center h-full gap-5 py-8"></div>
+      <div className="flex flex-col items-center justify-center h-full gap-5 py-8">
+        <TableContainer />
+      </div>
     </div>
   );
 };
