f88f4fbe75774120a654b1710ca250c6c57ac92f
0-BSCode <bryanipsanchez@gmail.com>
2023-11-29 21:22:43 +0800

    Remove unused import

--- client/src/pages/document/components/DocumentComments.tsx
+++ client/src/pages/document/components/DocumentComments.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { Button, Input, Space, Typography } from 'antd';
 import useComments from '@src/hooks/useComments';
 import { useParams } from 'react-router-dom';
