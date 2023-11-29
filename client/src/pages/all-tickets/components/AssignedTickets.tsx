3c3affb31a5193d2103d10843ff5604ca03afa66
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-29 12:17:10 +0800

    Create assignee email filter

--- client/src/pages/all-tickets/components/AssignedTickets.tsx
+++ client/src/pages/all-tickets/components/AssignedTickets.tsx
@@ -15,28 +15,31 @@ const AssignedTickets = (): JSX.Element => {
   const [filteredTickets, setFilteredTickets] = useState<TicketInterface[]>(allTickets);
 
   useEffect(() => {
-    if (titleFilter.length > 3 || dropDownFilter !== StatusEnum.ALL) {
+    if (titleFilter.length > 3 || assigneeFilter.length > 3 || dropDownFilter !== StatusEnum.ALL) {
       setFiltered(true);
 
       setFilteredTickets(
         allTickets.filter(
           (tic) =>
             (titleFilter.length > 3 ? tic.title.toLowerCase().includes(titleFilter.toLowerCase()) : true) &&
+            (assigneeFilter.length > 3
+              ? tic.assignee?.email.toLowerCase().includes(assigneeFilter.toLowerCase())
+              : true) &&
             (dropDownFilter === StatusEnum.ALL ? true : tic.status === dropDownFilter) &&
             tic.assigneeId,
         ),
       );
     } else {
       setFiltered(false);
-      setFilteredTickets(allTickets.filter((doc) => doc.assigneeId));
+      setFilteredTickets(allTickets.filter((doc) => doc.assigneeId !== null));
     }
-  }, [titleFilter, dropDownFilter]);
+  }, [titleFilter, assigneeFilter, dropDownFilter]);
 
   return (
     <div className="w-[90%] h-[45%] bg-white-100 flex flex-col gap-2 px-[2rem] py-[1.3rem]">
       <h1 className="m-0 text-xl font-bold">All Tickets</h1>
       <div className="flex flex-col ">
-        <div className="flex gap-5">
+        <div className="flex gap-[2%]">
           <div className="flex  flex-col justify-start w-[35%]">
             <p className="my-2 font-semibold ">Search by Title</p>
             <Input
@@ -63,7 +66,7 @@ const AssignedTickets = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <TicketsTable documents={filtered ? filteredTickets : allTickets.filter((doc) => doc.assigneeId)} />
+      <TicketsTable documents={filtered ? filteredTickets : allTickets.filter((doc) => doc.assigneeId !== null)} />
     </div>
   );
 };
