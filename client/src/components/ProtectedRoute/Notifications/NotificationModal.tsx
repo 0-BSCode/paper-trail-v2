e3e362a25df08338960eec04e14ef0b259840324
Luzefiru <ian.deejay.iv@gmail.com>
2023-11-30 20:23:14 +0800

    Integrated notification backend with frontend

--- client/src/components/ProtectedRoute/Notifications/NotificationModal.tsx
+++ client/src/components/ProtectedRoute/Notifications/NotificationModal.tsx
@@ -1,49 +1,62 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import useAuth from '@src/hooks/useAuth';
-import { Modal, Button, Avatar, List } from 'antd';
-import { BellOutlined } from '@ant-design/icons';
+import { Modal, Button, Avatar, List, Spin } from 'antd';
+import { BellOutlined, InfoCircleFilled } from '@ant-design/icons';
+import { ToastContext } from '@src/context/ToastContext';
+import NotificationService from '@src/services/notification-service';
+import type Notification from '@src/types/interfaces/notification';
 
 const NotificationModal = (): JSX.Element => {
+  const { userId, accessToken } = useAuth();
+  const { error, success } = useContext(ToastContext);
   const [isNotificationsModalOpen, setIsNotificationsModalOpen] = useState(false);
-  const { userId } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [notifications, setNotifications] = useState<Notification[] | undefined>(undefined);
 
-  const handleOpenNotificationsModal = (): void => {
+  const fetchNotifications = async (): Promise<void> => {
+    setIsLoading(true);
+    const fetchedNotifications = await NotificationService.getNoticications(accessToken as string);
+    setNotifications(
+      fetchedNotifications.sort((a, b) => {
+        const timeA = new Date(a.createdAt).getTime();
+        const timeB = new Date(b.createdAt).getTime();
+        return timeA === timeB ? 0 : timeA < timeB ? 1 : -1;
+      }),
+    );
+    setIsLoading(false);
+  };
+
+  const handleOpenNotificationsModal = async (): Promise<void> => {
     setIsNotificationsModalOpen(true);
-    // TODO (Ian): Add logic to fetch from backend and use Spinner component for isLoading
-    console.log('Fetching notifications for userId:', userId);
+
+    await fetchNotifications();
   };
 
   const handleClose = (): void => {
     setIsNotificationsModalOpen(false);
   };
 
-  const handleMarkAsRead = (): void => {
-    console.log('Marking notifications as read...');
-    // TODO (Ian): Integrate with Toaster implementation and handle backend mutation logic
-    window.alert('This should be a toast: "Successfully marked notifications as read."');
-  };
+  const handleMarkAsRead = async (): Promise<void> => {
+    if (userId === null || accessToken === null) {
+      error('Please log in.');
+      return;
+    }
 
-  // TODO (Ian): Remove once integrated with backend
-  const mockData = [
-    {
-      message: `Your ticket "I Want to Appeal My Grades" has a new comment from christian.dejesus: "The teacher has approved this and will act accordingly".`,
-      isRead: false,
-      createdAt: '2023-11-17 08:43:47',
-    },
-    {
-      message: `Your ticket "I Want to Appeal My Grades" has a new comment from christian.dejesus: "The teacher has approved this and will act accordingly".`,
-      isRead: false,
-      createdAt: '2023-10-17 08:43:47',
-    },
-    {
-      message: `Your ticket "I Want to Appeal My Grades" has a new comment from christian.dejesus: "The teacher has approved this and will act accordingly".`,
-      isRead: false,
-      createdAt: '2022-11-17 08:43:47',
-    },
-  ];
+    if (notifications === undefined) {
+      return;
+    }
+
+    for (let i = 0; i < notifications?.length; i++) {
+      await NotificationService.markNotificationAsRead(accessToken, notifications[i].id);
+    }
+    success('Successfully marked notifications as read.');
+
+    void fetchNotifications();
+  };
 
   return (
     <>
+      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
       <Button type="link" size="middle" onClick={handleOpenNotificationsModal}>
         <BellOutlined />
       </Button>
@@ -51,28 +64,36 @@ const NotificationModal = (): JSX.Element => {
         title="Notifications"
         open={isNotificationsModalOpen}
         okText={'Mark as Read'}
+        /* eslint-disable-next-line @typescript-eslint/no-misused-promises */
         onOk={handleMarkAsRead}
         cancelText={'Close'}
         onCancel={handleClose}
         width={1000}
       >
-        <List
-          itemLayout="horizontal"
-          dataSource={mockData}
-          renderItem={(item, index) => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={<Avatar size={64} src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />}
-              />
-              <div className="flex items-center ml-2">
-                <div>
-                  <p className="text-lg">{item.message}</p>
-                  <p className="font-semibold text-end">{formatTimeAgo(item.createdAt)}</p>
+        {isLoading ? (
+          <Spin size="large" />
+        ) : (
+          <List
+            itemLayout="horizontal"
+            dataSource={notifications}
+            renderItem={(item, index) => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar size={64} src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />}
+                />
+                <div className="flex flex-col w-full ml-2">
+                  <div>
+                    <p className="w-full text-lg">
+                      {!item.isRead && <InfoCircleFilled className="text-[#ce3131] me-2" />}
+                      {item.message}
+                    </p>
+                    <p className="font-semibold text-end">{formatTimeAgo(item.createdAt)}</p>
+                  </div>
                 </div>
-              </div>
-            </List.Item>
-          )}
-        />
+              </List.Item>
+            )}
+          />
+        )}
       </Modal>
     </>
   );
