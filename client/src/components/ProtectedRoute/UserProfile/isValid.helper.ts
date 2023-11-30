bb6956d444963c022c00194bf89c3531c2b2d313
Luzefiru <ian.deejay.iv@gmail.com>
2023-11-30 18:40:56 +0800

    Fix isValid.fullName

--- client/src/components/ProtectedRoute/UserProfile/isValid.helper.ts
+++ client/src/components/ProtectedRoute/UserProfile/isValid.helper.ts
@@ -1,20 +1,26 @@
+/**
+ * Collection of helper methods to do input validation for the UserProfileModal component.
+ *
+ * Empty fields are considered valid because they are optionally filled out.
+ */
 const isValid = {
   fullName: (fullName: string): boolean => {
+    if (fullName === '') return true;
     const fullNamePattern = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/; // Matches names with spaces and common punctuation
     return fullNamePattern.test(fullName);
   },
   studentNumber: (studentNumber: string): boolean => {
-    if (studentNumber === '') return true; // Empty string is considered valid
+    if (studentNumber === '') return true;
     const studentNumberPattern = /^[0-9]{8}$/; // Matches 8 digits
     return studentNumberPattern.test(studentNumber);
   },
   contactNumber: (contactNumber: string): boolean => {
-    if (contactNumber === '') return true; // Empty string is considered valid
+    if (contactNumber === '') return true;
     const contactNumberPattern = /^\+[1-9]\d{7,14}$/; // Matches international phone number format
     return contactNumberPattern.test(contactNumber);
   },
   courseAndYear: (courseAndYear: string): boolean => {
-    if (courseAndYear === '') return true; // Empty string is considered valid
+    if (courseAndYear === '') return true;
     const courseAndYearPattern = /^[A-Z]{4}-[1-9]$/; // Matches format XXXX-N, where XXXX is 4 uppercase letters and N is a single digit from 1 to 9
     return courseAndYearPattern.test(courseAndYear);
   },
