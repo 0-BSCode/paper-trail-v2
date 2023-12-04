581d0bb003d19ca6e7f1687275226f6758e18f94
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-12-04 23:08:25 +0800

    Refactored ProtectedRoute header

--- client/src/components/ProtectedRoute/index.tsx
+++ client/src/components/ProtectedRoute/index.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useRef, useContext } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import useAuth from '@src/hooks/useAuth';
-import { Layout, Flex, Image, Typography, Button } from 'antd';
-import LogoWhite from '@src/assets/logo-white.svg';
+import { Layout, Flex, Button } from 'antd';
 import { ToastContext } from '@src/context/ToastContext';
 import { NotificationModal } from './Notifications';
 import { UserProfileModal } from './UserProfile';
+import WordmarkLogo from '../WordmarkLogo';
 
 const { Header, Content } = Layout;
-const { Title } = Typography;
+
 interface AuthRouteProps {
   element: JSX.Element;
 }
@@ -40,18 +40,7 @@ const ProtectedRoute = ({ element }: AuthRouteProps): JSX.Element => {
         <Layout className="h-screen">
           <Header className="h-fit">
             <Flex className="items-center justify-between">
-              <div className="flex items-center gap-4">
-                <Image width={30} src={LogoWhite} alt="Wordmark Logo of Paper Trail" />
-                <Title
-                  style={{
-                    color: 'white',
-                    marginTop: 1,
-                    marginBottom: 5,
-                  }}
-                >
-                  Paper Trail
-                </Title>
-              </div>
+              <WordmarkLogo />
               <div className="flex gap-1">
                 <NotificationModal />
                 <UserProfileModal />
--- client/src/components/WordmarkLogo/index.tsx
+++ client/src/components/WordmarkLogo/index.tsx
@@ -1,10 +1,11 @@
 import LogoWhite from '@src/assets/logo-white.svg';
+import { Image } from 'antd';
 
 function WordmarkLogo(): JSX.Element {
   return (
-    <div className="flex items-center gap-4 text-2xl font-medium">
-      <img className="w-8" src={LogoWhite} alt="Logo" />
-      Paper Trail
+    <div className="flex items-center gap-4 text-2xl font-medium text-center">
+      <Image width={30} src={LogoWhite} alt="Wordmark Logo of Paper Trail" />
+      <h1 className="text-3xl font-bold text-white">Paper Trail</h1>
     </div>
   );
 }
