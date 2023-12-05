a3485d5f4225ca6db38c08cb321d6e6c65ac55c5
0-BSCode <bryanipsanchez@gmail.com>
2023-12-06 00:51:46 +0800

    Remove unused seeder data

--- server/src/db/seeders/2023.09.14T13.55.59.add-users.ts
+++ server/src/db/seeders/2023.09.14T13.55.59.add-users.ts
@@ -7,9 +7,6 @@ const seedUsers = [
     password: "$2b$10$URq.WnApWY.iNnZMQa4jcOlC/hjnZR4wv.1E1sN2wYI088XVnx.O2",
     student_id_number: "20101466",
     full_name: "Bryan Ignatius Sanchez",
-    is_verified: true,
-    verification_token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyeWFuLnNhbmNoZXpAdXNjLmVkdS5waCIsImlhdCI6MTY5ODc1NTI5MH0.IGwviF7-2Ap8cwcQRJLS13OlwC2XXezlnjYPhpYNiHk",
     created_at: new Date(),
     updated_at: new Date()
   },
@@ -19,9 +16,6 @@ const seedUsers = [
     password: "$2b$10$i3KaIbiHdt.6Ejv9kMJlh.oAS9gBnWXdQmmAZp7Z9JoKb8nNy76lG",
     student_id_number: "22101295",
     full_name: "Christian Antonio IV L. de Jesus",
-    is_verified: true,
-    verification_token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNocmlzdGlhbi5kZWplc3VzQHVzYy5lZHUucGgiLCJpYXQiOjE2OTg3NTE4NzF9.5yuw4cQims03cac7L0lQlv4QGKnMgCcWJiHeImyXA6Q",
     created_at: new Date(),
     updated_at: new Date()
   },
@@ -31,9 +25,6 @@ const seedUsers = [
     password: "$2b$10$f5MFKbiBoFzMk7fwbnN0AebCP9NaMXLQcMLU42HGiHX8FaKCeN9MS",
     student_id_number: "20103261",
     full_name: "Samuel Ethan Bonghanoy",
-    is_verified: true,
-    verification_token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbXVlbC5ib25naGFub3lAdXNjLmVkdS5waCIsImlhdCI6MTY5ODc1MTk0N30.qWtaVw165CUFFnjLt7Gy2xQavT5jl3bumcE1KvXMX4w",
     created_at: new Date(),
     updated_at: new Date()
   },
@@ -43,9 +34,6 @@ const seedUsers = [
     password: "$2b$10$eVR88f0BKGpyNLNXWY47deVuYRn5WSk7AwS.sk98wG8HQ8cOsO4jS",
     student_id_number: "20103994",
     full_name: "Fitzsixto Angelo Singh",
-    is_verified: true,
-    verification_token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZpdHpzaXh0by5zaW5naEB1c2MuZWR1LnBoIiwiaWF0IjoxNjk4NzUxOTU3fQ.cyx-F0_5M73tTkFZ8EwGmWVAZ-Ih-Ip6i66qRkgd94w",
     created_at: new Date(),
     updated_at: new Date()
   }
