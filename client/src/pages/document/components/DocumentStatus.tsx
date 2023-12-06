a1f443cbf8472d6007b8cd7982f08735ad1cd768
0-BSCode <bryanipsanchez@gmail.com>
2023-12-06 16:47:26 +0800

    Add additional helper text for status

--- client/src/pages/document/components/DocumentStatus.tsx
+++ client/src/pages/document/components/DocumentStatus.tsx
@@ -115,6 +115,7 @@ const DocumentStatus = ({ documentId }: { documentId: string }): JSX.Element => 
         <Typography.Text type="danger">
           * {statusToHelperTextMapping[status as keyof typeof statusToHelperTextMapping]}
         </Typography.Text>
+        <Typography.Text type="secondary">* Please leave a comment before updating</Typography.Text>
       </Space.Compact>
       <Button disabled={disableButton} onClick={saveStatus}>
         Save Status
